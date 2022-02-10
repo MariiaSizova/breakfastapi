504692a42b60c23f1f32a076158b62bee5e57c83
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-02-10 17:35:37 -0500

    Add Recipe model for openapi docs
    Add Recipe model and response type so the openapi docs can show examples

--- main.py
+++ main.py
@@ -1,20 +1,43 @@
 import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 from db.database import fetch_random_recipe, fetch_recipe_by_id
+
+
+class Recipe(BaseModel):
+    id: int
+    name: str
+    total_duration: int
+    ingredients: list
+    directions: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "id": 5490,
+                "name": "Tongue and Mustard Sandwiches Recipe",
+                "total_duration": 150,
+                "ingredients": ["salt", "onion", "mustard", "bread", "beef"],
+                "directions": "Rinse beef tongue and place in a large pot. Cover with water and add the salt and chopped onion...",
+            }
+        }
+
 
 app = FastAPI()
 
 
-@app.get("/", status_code=200)
+@app.get("/", status_code=200, response_model=Recipe)
 def get_recipes() -> dict:
-    return {"status": 200, "recipe": fetch_random_recipe()}
+    recipe: Recipe = fetch_random_recipe()
+    return recipe
 
 
-@app.get("/{id}", status_code=200)
+@app.get("/{id}", status_code=200, response_model=Recipe)
 def get_recipe_by_id(id) -> dict:
-    if fetch_recipe_by_id(id) is not None:
-        return {"status": 200, "recipe": fetch_recipe_by_id(id)}
+    recipe: Recipe = fetch_recipe_by_id(id)
+    if recipe is not None:
+        return {"status": 200, "recipe": recipe}
     return {"status": 404, "message": "recipe not found"}
 
 
