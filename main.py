0b1394ccea4ba8c466926a1979a0fed4bd1a7c99
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-02-10 17:06:34 -0500

    Add id path parameter to search recipe by id

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 
-from db.database import fetch_random_recipe
+from db.database import fetch_random_recipe, fetch_recipe_by_id
 
 app = FastAPI()
 
@@ -9,6 +9,13 @@ app = FastAPI()
 @app.get("/", status_code=200)
 def get_recipes() -> dict:
     return {"status": 200, "recipe": fetch_random_recipe()}
+
+
+@app.get("/{id}", status_code=200)
+def get_recipe_by_id(id) -> dict:
+    if fetch_recipe_by_id(id) is not None:
+        return {"status": 200, "recipe": fetch_recipe_by_id(id)}
+    return {"status": 404, "message": "recipe not found"}
 
 
 if __name__ == "__main__":
