cdc514039a0d67f240d742c061a631f083250c7d
obtusedev <66740598+obtusedev@users.noreply.github.com>
2022-02-10 16:29:52 -0500

    Refactor Database
    Change Database class into function for simplicity
    
    Return ingredients as python list rather than a string representation of a list
    since this makes it easier to manipulate the data
    
    Change dict keys to lowercase as most api response keys are in lowercase
    
    Change dict key "Cook Time (Minutes)" to "total_duration" as this makes it much
    easier to select the key
    
    data["total_duration"] rather than data["Cook Time (Minutes)"]

--- db/database.py
+++ db/database.py
@@ -1,12 +1,23 @@
 import sqlite3
+import json
 
 
-class Database:
-    def __init__(self):
-        self.connection = sqlite3.connect("db/recipes.db", check_same_thread=False)
-        self.cursor = self.connection.cursor()
+def fetch_random_recipe() -> dict:
+    """
+    Returns a recipe dict.
+    """
+    with sqlite3.connect("db/recipes.db", check_same_thread=False) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM items ORDER BY RANDOM() LIMIT 1")
+        data = cursor.fetchone()
+        return {
+            "id": data[0],
+            "name": normalize_string(data[1]),
+            "total_duration": data[2],
+            "ingredients": json.loads(
+                data[3].replace("'", '"')
+            ),  # convert string list to python list
+            "directions": data[4],
+        }
 
-    def get_data(self):
-        self.cursor.execute("SELECT * FROM items ORDER BY RANDOM() LIMIT 1")
-        return self.cursor.fetchone()
 
