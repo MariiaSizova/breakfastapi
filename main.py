272102df4e7cd69c757255b8bbdc81f6d3e2ff68
nakyamurya <nakyamurya@gmail.com>
2022-02-19 12:05:25 -0500

    activate auto reload

--- main.py
+++ main.py
@@ -43,4 +43,4 @@ def get_recipe_by_id(id: int) -> dict:
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
